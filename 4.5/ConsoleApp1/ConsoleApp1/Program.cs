918bce892a464c7f9276a35d874223ff2b745ec2
Максим Стрелец <1megamaks1@gmail.com>
2017-11-30 19:14:23 +0200

    Chapter 5 done

---
+++ 4.5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num;
+            Func<int> func1 = () => 42;
+            Func<int, int> funk2 = (Func<int, int>)(object)func1;
+            var a = funk2(13);
+        }
+    }
+}
---
+++ 5.3/Lab5.3.1/Lab5.3.1/Lab531.cpp
@@ -0,0 +1,46 @@
+// Lab531.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+class FlightBooking {
+public:
+	FlightBooking(int id, int capacity, int reserved);
+	void printStatus();
+private:
+	int id;
+	int capacity;
+	int reserved;
+};
+
+void FlightBooking::printStatus()
+{
+	std::cout.precision(0);
+	std::cout << "Flight " << id << " : " << reserved << "/" << capacity << " (" << std::fixed << (float)reserved / capacity * 100 << "%) seats taken";
+}
+
+FlightBooking::FlightBooking(int id, int capacity, int reserved)
+{
+	this->id = id;
+	this->capacity = capacity;
+	this->reserved = reserved;
+}
+
+int main() 
+{
+	int reserved = 0,
+		capacity = 0;
+	std::cout << "Provide flight capacity: ";
+	std::cin >> capacity;
+
+	std::cout << "Provide number of reserved seats: ";
+	std::cin >> reserved;
+
+	FlightBooking booking(1, capacity, reserved);
+
+	booking.printStatus();
+
+	return 0;
+}
+

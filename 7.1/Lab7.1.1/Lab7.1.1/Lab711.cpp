995077a122ff710f468d101a291dce52522de0e7
Maksim <maksim.strelets98@gmail.com>
2017-12-18 15:24:08 +0200

    Chapter 7 done

---
+++ 7.1/Lab7.1.1/Lab7.1.1/Lab711.cpp
@@ -0,0 +1,26 @@
+// Lab711.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void) {
+	int a = 8, b = 0, c = 0;
+	while (cin >> b) {
+		try {
+			//Your code here
+			if (b == 0)
+				throw string("divide   by 0");
+			c = a / b;
+			cout << c << endl;
+		}
+		catch (string e) {
+			cout << e << endl;
+		}
+	}
+
+	// Your code here
+
+	return 0;
+}
